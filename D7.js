5f4fcea54fd8ececde690ee490c1e7239654af57
MichelaKalowski <mi.ky90@hotmail.com>
2022-12-14 19:53:06 +0100

    aggiornamento

--- D7.js
+++ D7.js
@@ -2,12 +2,12 @@
   Scrivi una funzione per concatenare due stringhe ricevute come parametri, selezionando solamente i primi 2 caratteri della
   prima e gli ultimi 3 della seconda. Converti la stringa risultante in maiuscolo e mostrala con un console.log().
 */
-function firstTwoandLasthree(string1, string2){
-let firstTwo= string1.substring(0,2);
-let Lastthree= string2.substring(string2.lenght=3);
-return result;
+function firstTwoandLasthree(string1, string2) {
+  let firstTwo = string1.substring(0, 2);
+  let Lastthree = string2.substr(-3);
+  return (firstTwo + Lastthree).toUpperCase();
 }
-console.log(firstTwoandLasthree("Ciao","Michela"))
+console.log("esercizio1: " + firstTwoandLasthree("Ciao", "Michela"))
 
 
 /* ESERCIZIO 2
@@ -20,48 +20,98 @@ function numbers() {
     random.push(randomnum);
 
   }
-  console.log(random);
+
   return random;
 }
-numbers();
+console.log("esercizio2: " + numbers())
 
 
 /* ESERCIZIO 3
   Scrivi una funzione per ricavare solamente i valori PARI da un array composto da soli valori numerici (suggerimento: il metodo filter può aiutare)
 */
-const numeri = [1, 2, 3, 4, 5, 6,];
-const pari = numeri.filter(numeri => numeri % 2 === 0);
-console.log(pari);
+
+
+const numeri = [1, 2, 3, 4, 5, 6];
+function estrai_pari(numeri) {
+  const pari = numeri.filter(numeri => numeri % 2 === 0);
+  return pari;
+}
+
+console.log("esercizio3: " + estrai_pari(numeri));
 
 /* ESERCIZIO 4
   Scrivi una funzione per sommare i numeri contenuti in un array
 */
-const array = [1, 2, 3, 4, 5];
-let sum = 0;
-for (let i = 0; i < array - length; i++) {
-  sum += array[i]
 
+const array = [1, 2, 3, 4, 5];
+function somma_numeri(arr) {
+
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  return sum;
 }
-console - log(sum)
+console.log("esercizio4: " + somma_numeri(array));
 
 /* ESERCIZIO 5
   Scrivi una funzione per sommare i numeri contenuti in un array (usare REDUCE)
 */
+function somma_numeri2(arr) {
+  const initialValue = 0;
+  let somma = arr.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
+  return somma;
+}
 
-
+console.log("esercizio5: " + somma_numeri2(array));
 
 /* ESERCIZIO 6
   Scrivi una funzione che, dato un array di soli numeri e un numero n come parametri, ritorni un secondo array con tutti i valori del precedente incrementati di n
 */
 
+function somma_n(arr, n) {
+  const arr_ret = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr_ret.push(arr[i] + n);
+
+
+  }
+  return arr_ret;
+
+}
+console.log("esercizio6: " + somma_n(array, 3));
+
 /* ESERCIZIO 8
   Scrivi una funzione che, dato un array di stringhe, ritorni un nuovo array contenente le lunghezze delle rispettive stringhe dell'array di partenza
   es.: ["EPICODE", "is", "great"] => [7, 2, 5]
 */
+const array_s = ["EPICODE", "IS", "great"];
+function somma_s(arr) {
+  const arr_ret = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr_ret.push(arr[i].length);
+  }
+  return arr_ret;
+
+
+}
+
+console.log("esercizio8: " + somma_s(array_s));
+
 
 /* ESERCIZIO 9
-  Scrivi una funzione per creare un array contenente tutti i valori DISPARI dP0+ a 99.
+  Scrivi una funzione per creare un array contenente tutti i valori DISPARI da 0 a 99.
 */
+function num_dispari() {
+  let arr_ret = [];
+  for (let i = 0; i <=100; i++) {
+    if (i % 2 != 0)
+     arr_ret.push(i);
+
+}
+return arr_ret;
+}
+console.log("esercizio9:"+ num_dispari());
 
 /* Questo array di film verrà usato negli esercizi a seguire. Non modificarlo e scorri oltre per riprendere gli esercizi :) */
 const movies = [
